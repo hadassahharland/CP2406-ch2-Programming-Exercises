4e03b4860ed7667b742327117797c55445c2d034
Dassa <hadassah.harland@gmail.com>
2016-08-08 08:30:18 +1000

    Exercise 4a. Complete

---
+++ Programming Exercises/src/QuartsToGallons.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Dassa on 8/08/2016.
+ * A class to convert a volume in quarts to gallons
+ */
+public class QuartsToGallons
+{
+    public static void main(String[] args)
+    {
+        int conversion = 4;                 /* Conversion rate */
+        int quarts = 18;                    /* Amount to be converted in quarts */
+        int gallons = quarts/conversion;                 /* Number of complete Gallons */
+        int remainder = 18 - (gallons*4);   /* Remaining quarts */
+        System.out.println("A job that requires " + quarts + " quarts requires " + gallons + " gallons plus " + remainder + " quarts.");
+
+    }
+
+}
